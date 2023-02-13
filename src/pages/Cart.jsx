55f3eb901daf3255eb3a86fa3e857c17a04f31ec
nasiru hammed aimanosi <nasaces360@gmail.com>
2023-02-13 01:01:02 +0100

    Added mobile responsiveness

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,11 +3,13 @@ import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 
 const Wrapper = styled.div`
   padding: 20px;
+  ${mobile({ padding: "10px" })}
 `;
 
 const Title = styled.h1`
@@ -32,7 +34,9 @@ const TopButton = styled.button`
   color: ${(props) => props.type === "filled" && "white"};
 `;
 
-const TopTexts = styled.div``;
+const TopTexts = styled.div`
+  ${mobile({ display: "none" })}
+`;
 
 const TopText = styled.span`
   text-decoration: underline;
@@ -43,6 +47,7 @@ const TopText = styled.span`
 const Bottom = styled.div`
   display: flex;
   justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const Info = styled.div`
@@ -52,6 +57,7 @@ const Info = styled.div`
 const Product = styled.div`
   display: flex;
   justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
 `;
 
 const ProductDetail = styled.div`
@@ -89,22 +95,32 @@ const PriceDetail = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  ${mobile({
+    flexDirection: "row",
+    justifyContent: "space-around",
+    margin: "15px 0",
+  })}
 `;
 
 const ProductAmountContainer = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 20px;
+  background-color: #f1f1f1;
+  padding: 5px 20px;
+  ${mobile({ marginBottom: "0px", padding: "5px 20px" })}
 `;
 
 const ProductAmount = styled.div`
   font-size: 24px;
   margin: 5px;
+  padding: 0px 30px;
 `;
 
 const ProductPrice = styled.div`
   font-size: 30px;
-  font-weight: 200;
+  font-weight: 400;
+  ${mobile({ top: "0px" })}
 `;
 
 const Hr = styled.hr`
@@ -128,8 +144,8 @@ const SummaryItem = styled.div`
   margin: 30px 0;
   display: flex;
   justify-content: space-between;
-  font-weight: ${(props) => props.type === "total" && "500"};
-  font-size: ${(props) => props.type === "total" && "24"};
+  font-weight: ${(props) => props.type === "total" && "800"};
+  font-size: ${(props) => props.type === "total" && "24px"};
 `;
 const SummaryItemText = styled.span``;
 
