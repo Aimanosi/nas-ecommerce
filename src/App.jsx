e32a62db60e9bf40fb019b9ec790453eae2b31f1
nasiru hammed aimanosi <nasaces360@gmail.com>
2023-05-07 23:45:38 +0100

    add routes and filter functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 const App = () => {
-  return <Home />;
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/products/:category" element={<ProductList />}></Route>
+        <Route path="/product/:id" element={<Product />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/" /> : <Login />}
+        ></Route>
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        ></Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
