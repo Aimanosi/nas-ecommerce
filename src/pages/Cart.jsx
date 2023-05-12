b19161d8e7e458b77d11cad22612dbddf679dc2a
nasiru hammed aimanosi <nasaces360@gmail.com>
2023-05-12 01:05:53 +0100

    implement stripe payment

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,7 +6,9 @@ import Navbar from "../components/Navbar";
 import { mobile } from "../responsive";
 import { useSelector } from "react-redux";
 import StripeCheckout from "react-stripe-checkout";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { userRequest } from "../requestMethods";
+import { useNavigate } from "react-router-dom";
 
 const KEY = process.env.REACT_APP_STRIPE;
 
@@ -167,12 +169,25 @@ const Button = styled.button`
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
   const [stripeToken, setStripeToken] = useState(null);
+  const history = useNavigate();
 
   const onToken = (token) => {
     setStripeToken(token);
   };
 
-  console.log(stripeToken);
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await userRequest.post("/checkout/payment", {
+          tokenId: stripeToken.id,
+          amount: 500,
+        });
+        history.push("/success", { data: res.data });
+      } catch {}
+    };
+    stripeToken && makeRequest();
+  }, [stripeToken, cart.total, history]);
+
   return (
     <Container>
       <Navbar />
@@ -250,6 +265,7 @@ const Cart = () => {
               amount={cart.total * 100}
               token={onToken}
               stripeKey={KEY}
+              // Add any other props you need
             >
               <Button>CHECKOUT NOW</Button>
             </StripeCheckout>
